89b267b07dd2cc107acc85d8c61765288621b932
Whales State <whalesstate@gmail.com>
2025-03-12 19:12:13 +0200

    Fix Crash when _emit_audio_enabled_change is called with an empty array.

--- youtube/youtube_playables_client.cpp
+++ youtube/youtube_playables_client.cpp
@@ -29,24 +29,24 @@
 /**************************************************************************/
 
 #include "youtube_playables_client.h"
-#include "youtube_playables_response.h"
 #include "platform/web/api/javascript_bridge_singleton.h"
+#include "youtube_playables_response.h"
 
 void YoutubePlayablesClient::_bind_methods() {
-    ClassDB::bind_method(D_METHOD("_emit_audio_enabled_change"), &YoutubePlayablesClient::_emit_audio_enabled_change);
-    ClassDB::bind_method(D_METHOD("_emit_pause"), &YoutubePlayablesClient::_emit_pause);
-    ClassDB::bind_method(D_METHOD("_emit_resume"), &YoutubePlayablesClient::_emit_resume);
+	ClassDB::bind_method(D_METHOD("_emit_audio_enabled_change"), &YoutubePlayablesClient::_emit_audio_enabled_change);
+	ClassDB::bind_method(D_METHOD("_emit_pause"), &YoutubePlayablesClient::_emit_pause);
+	ClassDB::bind_method(D_METHOD("_emit_resume"), &YoutubePlayablesClient::_emit_resume);
 
-    ClassDB::bind_method(D_METHOD("is_youtube_environment"), &YoutubePlayablesClient::is_youtube_environment);
-    ClassDB::bind_method(D_METHOD("get_sdk_version"), &YoutubePlayablesClient::get_sdk_version);
-    ClassDB::bind_method(D_METHOD("send_score", "value"), &YoutubePlayablesClient::send_score);
-    ClassDB::bind_method(D_METHOD("open_yt_content", "video_id"), &YoutubePlayablesClient::open_yt_content);
-    ClassDB::bind_method(D_METHOD("load_data"), &YoutubePlayablesClient::load_data);
-    ClassDB::bind_method(D_METHOD("save_data", "data"), &YoutubePlayablesClient::save_data);
-    ClassDB::bind_method(D_METHOD("log_error"), &YoutubePlayablesClient::log_error);
-    ClassDB::bind_method(D_METHOD("log_warning"), &YoutubePlayablesClient::log_warning);
-    ClassDB::bind_method(D_METHOD("is_audio_enabled"), &YoutubePlayablesClient::is_audio_enabled);
-    ClassDB::bind_method(D_METHOD("get_language"), &YoutubePlayablesClient::get_language);
+	ClassDB::bind_method(D_METHOD("is_youtube_environment"), &YoutubePlayablesClient::is_youtube_environment);
+	ClassDB::bind_method(D_METHOD("get_sdk_version"), &YoutubePlayablesClient::get_sdk_version);
+	ClassDB::bind_method(D_METHOD("send_score", "value"), &YoutubePlayablesClient::send_score);
+	ClassDB::bind_method(D_METHOD("open_yt_content", "video_id"), &YoutubePlayablesClient::open_yt_content);
+	ClassDB::bind_method(D_METHOD("load_data"), &YoutubePlayablesClient::load_data);
+	ClassDB::bind_method(D_METHOD("save_data", "data"), &YoutubePlayablesClient::save_data);
+	ClassDB::bind_method(D_METHOD("log_error"), &YoutubePlayablesClient::log_error);
+	ClassDB::bind_method(D_METHOD("log_warning"), &YoutubePlayablesClient::log_warning);
+	ClassDB::bind_method(D_METHOD("is_audio_enabled"), &YoutubePlayablesClient::is_audio_enabled);
+	ClassDB::bind_method(D_METHOD("get_language"), &YoutubePlayablesClient::get_language);
 
 	ADD_SIGNAL(MethodInfo("audio_enabled_change", PropertyInfo(Variant::BOOL, "enabled")));
 	ADD_SIGNAL(MethodInfo("pause"));
@@ -54,142 +54,144 @@ void YoutubePlayablesClient::_bind_methods() {
 }
 
 void YoutubePlayablesClient::_emit_audio_enabled_change(Array p_array) {
-    audio_enabled = p_array[0];
-    emit_signal("audio_enabled_change", p_array[0]);
+	ERR_FAIL_COND(p_array.is_empty());
+
+	audio_enabled = p_array[0];
+	emit_signal("audio_enabled_change", p_array[0]);
 }
 
 void YoutubePlayablesClient::_emit_pause() {
-    emit_signal("pause");
+	emit_signal("pause");
 }
 
 void YoutubePlayablesClient::_emit_resume() {
-    emit_signal("resume");
+	emit_signal("resume");
 }
 
 void YoutubePlayablesClient::log_error() {
-    if (is_youtube_environment()) {
-        ytgameRef->call("logError");
-    }
+	if (is_youtube_environment()) {
+		ytgameRef->call("logError");
+	}
 }
 
 void YoutubePlayablesClient::log_warning() {
-    if (is_youtube_environment()) {
-        ytgameRef->call("logWarning");
-    }
+	if (is_youtube_environment()) {
+		ytgameRef->call("logWarning");
+	}
 }
 
 bool YoutubePlayablesClient::is_audio_enabled() {
-    return audio_enabled;
+	return audio_enabled;
 }
 
 bool YoutubePlayablesClient::is_youtube_environment() {
-    if (ytgameRef.is_valid()) {
-        return ytgameRef->call("isYoutubePlayables");
-    }
-    return false;
+	if (ytgameRef.is_valid()) {
+		return ytgameRef->call("isYoutubePlayables");
+	}
+	return false;
 }
 
 String YoutubePlayablesClient::get_sdk_version() {
-    if (is_youtube_environment()) {
-        return ytgameRef->call("getSdkVersion");
-    }
-    return String();
+	if (is_youtube_environment()) {
+		return ytgameRef->call("getSdkVersion");
+	}
+	return String();
 }
 
 Ref<YoutubePlayablesResponse> YoutubePlayablesClient::load_data() {
-    Ref<YoutubePlayablesResponse> response = Ref<YoutubePlayablesResponse>();
-    response.instantiate();
+	Ref<YoutubePlayablesResponse> response = Ref<YoutubePlayablesResponse>();
+	response.instantiate();
 
-    if (is_youtube_environment()) {
-        response->create_signal_finish_callback();
-        ytgameRef->call_deferred("loadData", response->get_signal_finish_callback());
-    } else {
+	if (is_youtube_environment()) {
+		response->create_signal_finish_callback();
+		ytgameRef->call_deferred("loadData", response->get_signal_finish_callback());
+	} else {
 		Callable callable = callable_mp(*response, &YoutubePlayablesResponse::_signal_finish_error);
 		callable.call_deferred("Not in Youtube Playables environment.");
-    }
-    return response;
+	}
+	return response;
 }
 
 Ref<YoutubePlayablesResponse> YoutubePlayablesClient::save_data(String p_data) {
-    Ref<YoutubePlayablesResponse> response = Ref<YoutubePlayablesResponse>();
-    response.instantiate();
+	Ref<YoutubePlayablesResponse> response = Ref<YoutubePlayablesResponse>();
+	response.instantiate();
 
-    if (is_youtube_environment()) {
-        response->create_signal_finish_callback();
-        ytgameRef->call_deferred("saveData", p_data, response->get_signal_finish_callback());
-    } else {
+	if (is_youtube_environment()) {
+		response->create_signal_finish_callback();
+		ytgameRef->call_deferred("saveData", p_data, response->get_signal_finish_callback());
+	} else {
 		Callable callable = callable_mp(*response, &YoutubePlayablesResponse::_signal_finish_error);
 		callable.call_deferred("Not in Youtube Playables environment.");
-    }
-    return response;
+	}
+	return response;
 }
 
 Ref<YoutubePlayablesResponse> YoutubePlayablesClient::send_score(int32_t p_value) {
-    Ref<YoutubePlayablesResponse> response = Ref<YoutubePlayablesResponse>();
-    response.instantiate();
+	Ref<YoutubePlayablesResponse> response = Ref<YoutubePlayablesResponse>();
+	response.instantiate();
 
-    if (is_youtube_environment()) {
-        response->create_signal_finish_callback();
-        ytgameRef->call_deferred("sendScore", p_value, response->get_signal_finish_callback());
-    } else {
+	if (is_youtube_environment()) {
+		response->create_signal_finish_callback();
+		ytgameRef->call_deferred("sendScore", p_value, response->get_signal_finish_callback());
+	} else {
 		Callable callable = callable_mp(*response, &YoutubePlayablesResponse::_signal_finish_error);
 		callable.call_deferred("Not in Youtube Playables environment.");
-    }
-    return response;
+	}
+	return response;
 }
 
 Ref<YoutubePlayablesResponse> YoutubePlayablesClient::open_yt_content(String p_video_id) {
-    Ref<YoutubePlayablesResponse> response = Ref<YoutubePlayablesResponse>();
-    response.instantiate();
+	Ref<YoutubePlayablesResponse> response = Ref<YoutubePlayablesResponse>();
+	response.instantiate();
 
-    if (is_youtube_environment()) {
-        response->create_signal_finish_callback();
-        ytgameRef->call_deferred("openYTContent", p_video_id, response->get_signal_finish_callback());
-    } else {
+	if (is_youtube_environment()) {
+		response->create_signal_finish_callback();
+		ytgameRef->call_deferred("openYTContent", p_video_id, response->get_signal_finish_callback());
+	} else {
 		Callable callable = callable_mp(*response, &YoutubePlayablesResponse::_signal_finish_error);
 		callable.call_deferred("Not in Youtube Playables environment.");
-    }
-    return response;
+	}
+	return response;
 }
 
 Ref<YoutubePlayablesResponse> YoutubePlayablesClient::get_language() {
-    Ref<YoutubePlayablesResponse> response = Ref<YoutubePlayablesResponse>();
-    response.instantiate();
+	Ref<YoutubePlayablesResponse> response = Ref<YoutubePlayablesResponse>();
+	response.instantiate();
 
-    if (is_youtube_environment()) {
-        response->create_signal_finish_callback();
-        ytgameRef->call_deferred("getLanguage", response->get_signal_finish_callback());
-    } else {
+	if (is_youtube_environment()) {
+		response->create_signal_finish_callback();
+		ytgameRef->call_deferred("getLanguage", response->get_signal_finish_callback());
+	} else {
 		Callable callable = callable_mp(*response, &YoutubePlayablesResponse::_signal_finish_error);
 		callable.call_deferred("Not in Youtube Playables environment.");
-    }
-    return response;
+	}
+	return response;
 }
 
 YoutubePlayablesClient::YoutubePlayablesClient() {
-    JavaScriptBridge *singleton = JavaScriptBridge::get_singleton();
-    if (!singleton) {
-        ERR_PRINT("JavaScriptBridge singleton is invalid");
-        return;
-    }
+	JavaScriptBridge *singleton = JavaScriptBridge::get_singleton();
+	if (!singleton) {
+		ERR_PRINT("JavaScriptBridge singleton is invalid");
+		return;
+	}
 
 	// YoutubePlayables defined in platform/web/export/export_plugin.cpp
-    if (!singleton->eval("window.YoutubePlayables?.isYoutubePlayables() ?? false", true)) {
-        return;
-    }
-    ytgameRef = singleton->get_interface("YoutubePlayables");
-    if (!ytgameRef.is_valid()) {
-        return;
-    }
+	if (!singleton->eval("window.YoutubePlayables?.isYoutubePlayables() ?? false", true)) {
+		return;
+	}
+	ytgameRef = singleton->get_interface("YoutubePlayables");
+	if (!ytgameRef.is_valid()) {
+		return;
+	}
 
-    on_audio_enabled_change_callback = singleton->create_callback(Callable(this, "_emit_audio_enabled_change"));
-    on_pause_callback = singleton->create_callback(Callable(this, "_emit_pause"));
-    on_resume_callback = singleton->create_callback(Callable(this, "_emit_resume"));
+	on_audio_enabled_change_callback = singleton->create_callback(Callable(this, "_emit_audio_enabled_change"));
+	on_pause_callback = singleton->create_callback(Callable(this, "_emit_pause"));
+	on_resume_callback = singleton->create_callback(Callable(this, "_emit_resume"));
 
-    ytgameRef->call("onAudioEnabledChange", on_audio_enabled_change_callback);
-    ytgameRef->call("onPause", on_pause_callback);
-    ytgameRef->call("onResume", on_resume_callback);
+	ytgameRef->call("onAudioEnabledChange", on_audio_enabled_change_callback);
+	ytgameRef->call("onPause", on_pause_callback);
+	ytgameRef->call("onResume", on_resume_callback);
 
-    // Needed to have is_audio_enabled return the right value
-    audio_enabled = singleton->eval("ytgame.system.isAudioEnabled()");
+	// Needed to have is_audio_enabled return the right value
+	audio_enabled = singleton->eval("ytgame.system.isAudioEnabled()");
 }
