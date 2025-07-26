ff89f0770e3bedd4d32ba2dda8e8295a93637328
Dragos Daian <daian.dragos@yahoo.com>
2025-07-26 20:14:56 +0200

    add config.py

---
+++ config.py
@@ -0,0 +1,42 @@
+@ -1,57 +0,0 @@
+def can_build(env, platform):
+	return True
+
+
+def configure(env):
+	pass
+
+
+def get_doc_classes():
+	return [
+		"LobbyClient",
+		"NetworkClient",
+		"LobbyInfo",
+		"LobbyPeer",
+		"LobbyResponse",
+		"LobbyResult",
+		"ViewLobbyResponse",
+		"ViewLobbyResult",
+		"ScriptedLobbyClient",
+		"ScriptedLobbyResponse",
+		"ScriptedLobbyResult",
+		"LoginClient",
+		"LoginURLResponse",
+		"LoginURLResult",
+		"LoginConnectResponse",
+		"LoginConnectResult",
+		"LoginVerifyTokenResponse",
+		"LoginVerifyTokenResult",
+		"LoginIDResponse",
+		"LoginIDResult",
+		"LoginAuthResponse",
+		"LoginAuthResult",
+		"ThirdPartyClient",
+		"DiscordEmbeddedAppClient",
+		"DiscordEmbeddedAppResponse",
+		"DiscordEmbeddedAppResult",
+	]
+
+
+def get_doc_path():
+	return "doc_classes"
